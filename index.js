80582b188fcb72ab80ebc4623204df5bb9b590e9
Ry Ferguson <rytf12@gmail.com>
2023-05-21 15:26:47 +0100

    add title and content placeholders to the main template file; add some hardcoded 'pages' data; loop over the 'pages' data and create a static HTML file for each

--- index.js
+++ index.js
@@ -2,4 +2,36 @@ const fs = require('fs')
 
 const templateText = fs.readFileSync('template.html', 'utf8')
 
-console.log(templateText)
+const pages = [
+    {
+        title: "Home",
+        content: "home content",
+        filename: 'index.html',
+    },
+    {
+        title: "Projects",
+        content: "projects content",
+        filename: 'projects.html'
+    },
+]
+
+//
+for(let i = 0; i < pages.length; ++i) {
+    const page = pages[i]
+    const html = templateText
+        .replace('{{title}}', page.title)
+        .replace('{{content}}', page.content)
+
+    fs.writeFile(
+        page.filename,
+        html,
+        error => {
+            if(error) {
+                //console.log(error)
+            }
+            else {
+                //console.log('saved')
+            }
+        }
+    )
+}
