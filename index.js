7ede042d211a61789cfb003ec741de70686ab0d6
Ry Ferguson <rytf12@gmail.com>
2025-01-16 19:58:55 +0000

    remove .html from nav links

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const templateText = fs.readFileSync('template.html', 'utf8')
 const pages = [
     {
         title: "Ry's Apps",
-        content: `Hello, I am a freelance software developer. I usually develop web apps. I have also built CLI apps, iOS apps, desktop apps, libraries and video games. You can see past projects on the <a href="projects.html">projects page</a>.`,
+        content: `Hello, I am a freelance software developer. I usually develop web apps. I have also built CLI apps, iOS apps, desktop apps, libraries and video games. You can see past projects on the <a href="/projects">projects page</a>.`,
         filename: 'index.html',
         nav_title: 'home',
         nav_url: '/',
@@ -16,14 +16,14 @@ const pages = [
         content: contentProjects(),
         filename: 'projects.html',
         nav_title: 'projects',
-        nav_url: 'projects.html',
+        nav_url: '/projects',
     },
     {
         title: "Contact",
         content: `<div style="text-align: center">Ry Ferguson<br><a href="mailto:rytf12@gmail.com">rytf12@gmail.com</a><br><a href="tel:9492732575">(949) 273-2575</a><br><a href="https://github.com/ferg1e">github.com/ferg1e</a></div>`,
         filename: 'contact.html',
         nav_title: 'contact',
-        nav_url: 'contact.html',
+        nav_url: '/contact',
         content_attrs: 'class="non-proj"'
     },
 ]
