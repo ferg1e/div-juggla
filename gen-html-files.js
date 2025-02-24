83baa6302d59ec2654659781ed19bfe7c0956b77
Ry Ferguson <rytf12@gmail.com>
2025-02-24 00:42:21 +0000

    put html file generation code in a function

---
+++ gen-html-files.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const {getNavItems} = require('./get-nav-items.js')
+
+function genHtmlFiles(pages, templateText) {
+    for(let i = 0; i < pages.length; ++i) {
+        const page = pages[i]
+    
+        const html = templateText
+            .replace('{{title}}', page.title)
+            .replace('{{content_attrs}}', page.content_attrs ? ' ' + page.content_attrs : '')
+            .replace('{{content}}', page.content)
+            .replace('{{nav_items}}', getNavItems(pages, i))
+    
+        fs.writeFile(
+            page.filename,
+            html,
+            error => {
+                if(error) {
+                    //console.log(error)
+                }
+                else {
+                    //console.log('saved')
+                }
+            }
+        )
+    }
+}
+
+module.exports.genHtmlFiles = genHtmlFiles
--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 const fs = require('fs')
 const pages = require('./pages.js')
-const {getNavItems} = require('./get-nav-items.js')
+const {genHtmlFiles} = require('./gen-html-files.js')
 
 const templateText = fs.readFileSync('template.html', 'utf8')
 
-//
-for(let i = 0; i < pages.length; ++i) {
-    const page = pages[i]
-
-    const html = templateText
-        .replace('{{title}}', page.title)
-        .replace('{{content_attrs}}', page.content_attrs ? ' ' + page.content_attrs : '')
-        .replace('{{content}}', page.content)
-        .replace('{{nav_items}}', getNavItems(pages, i))
-
-    fs.writeFile(
-        page.filename,
-        html,
-        error => {
-            if(error) {
-                //console.log(error)
-            }
-            else {
-                //console.log('saved')
-            }
-        }
-    )
-}
+genHtmlFiles(pages, templateText)
